8cc7ff0a0a834aecfd4580c324d0874992974a9f
sadeq <sdqSafdari@gmail.com>
2024-01-02 09:25:53 +0330

    Enrich Avro serializer, add both json encoding and binary encoding

--- src/main/java/AvroSerializerWithGenericRecord.java
+++ src/main/java/AvroSerializerWithGenericRecord.java
@@ -4,17 +4,20 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.io.JsonEncoder;
+import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
- * serialize generic record to avro json encoding(differs from plain json as it uses manifest typing)
+ * serialize a generic record to avro json encoding(differs from plain json as it uses manifest typing) and binary encoding
  * this example doesn't use the code generation, instead we use GenericRecord.
  *
  * GenericRecord uses the schema to verify that we only specify valid fields.
@@ -30,12 +33,28 @@ public class AvroSerializerWithGenericRecord {
         GenericData.Record avroRecord = new GenericRecordBuilder(schema).set("name", "sadeq").set("favourite_number", 7).build();
         GenericDatumWriter<GenericRecord> genericDatumWriter = new GenericDatumWriter<>(schema);
         try(ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()){
-            JsonEncoder jsonEncoder = EncoderFactory.get().jsonEncoder(schema, byteArrayOutputStream);// avro encodes to json or binary
+            BinaryEncoder binaryEncoder = binaryEncoder(byteArrayOutputStream);
+            genericDatumWriter.write(avroRecord,binaryEncoder);
+            binaryEncoder.flush();
+            logger.info("serialized binary datum: {}", Hex.encodeHexString(byteArrayOutputStream.toByteArray()));
+            byteArrayOutputStream.reset();
+            JsonEncoder jsonEncoder = jsonEncoder(byteArrayOutputStream, schema);
             genericDatumWriter.write(avroRecord,jsonEncoder);
             jsonEncoder.flush();
-            logger.info("serialized datum: {}", byteArrayOutputStream.toString());
+            logger.info("serialized json   datum: {}",byteArrayOutputStream.toString());
         }catch (IOException e){
             logger.error("avro serialization failed!",e);
+        }
+    }
+    private static BinaryEncoder binaryEncoder(OutputStream outputStream){
+        return EncoderFactory.get().binaryEncoder(outputStream, null);
+    }
+    private static JsonEncoder jsonEncoder(OutputStream outputStream,Schema schema){
+        try {
+            return EncoderFactory.get().jsonEncoder(schema,outputStream);
+        } catch (IOException e) {
+            logger.error("json encoder creation failed!",e);
+            throw new RuntimeException("JsonEncoder creation failed",e);
         }
     }
     private static Schema getFromSchemaFile(){
